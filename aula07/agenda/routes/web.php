cc326ab8ec5afe022ce59bcad2eb4f336160cd9b
Raíssa Sofka Mazzi <raissamazzi@gmail.com>
2026-05-05 19:54:48 -0300

    Finalização Agenda

--- aula07/agenda/routes/web.php
+++ aula07/agenda/routes/web.php
@@ -4,7 +4,8 @@ use App\Http\Controllers\ContatoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    //return view('welcome');
+    return redirect()->route('contatos.index');
 });
 
 Route::resource('contatos', ContatoController::class);
